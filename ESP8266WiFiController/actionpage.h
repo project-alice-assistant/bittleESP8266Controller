6ffc1ff0feb0b89b02f50c8bfb62b74a2aa2282a
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-04-18 18:31:03 +0200

    Full update

--- ESP8266WiFiController/actionpage.h
+++ ESP8266WiFiController/actionpage.h
@@ -3,69 +3,55 @@
 
 const char actionpage[] PROGMEM = R"=====(
 <div class="content">
-    <h4 class="center">Movements</h4>
-    <div class="btn center" style="width: 100%;">
-        <div class="radiolist">
-            Walk <input id="walk" name="movementType" type="radio" value="wk" checked>
+    <h2>Movements</h2>
+    <div class="card">
+        <div class="radio">
+            <label for="walk">Walk</label> <input id="walk" name="movementType" type="radio" value="wk" checked>
         </div>
-        <div class="radiolist">
-            Run <input id="run" name="movementType" type="radio" value="rn">
+        <div class="radio">
+            <label for="run">Run</label> <input id="run" name="movementType" type="radio" value="rn">
         </div>
-        <div class="radiolist">
-            Trot <input id="trot" name="movementType" type="radio" value="tr">
+        <div class="radio">
+            <label for="trot">Trot</label> <input id="trot" name="movementType" type="radio" value="tr">
         </div>
-        <div class="radiolist">
-            Crawl <input id="crawl" name="movementType" type="radio" value="cr">
+        <div class="radio">
+            <label for="crawl">Crawl</label> <input id="crawl" name="movementType" type="radio" value="cr">
         </div>
     </div>
-    <div class="btn center" style="width: 100%;">
-        <button onclick="movement('F')">Forward</button>
+    <div class="card">
+        <button onclick="movement('F')" style="width: 100%;">Forward</button>
+        <button onclick="movement('L')" style="width: 50%;">Forward Left</button>
+        <button onclick="movement('R')" style="width: 50%;">Forward Right</button>
+        <button onclick="location.href='/actionpage?cmd=kbkL'" style="width: 50%;">Back Left</button>
+        <button onclick="location.href='/actionpage?cmd=kbkR'" style="width: 50%;">Back Right</button>
+        <button onclick="location.href='/actionpage?cmd=kbk'" style="width: 100%;">Back</button>
     </div>
-    <div class="btn" style="width:100%">
-        <button onclick="movement('L')">Forward Left</button>
-        <button onclick="movement('R')">Forward Right</button>
+    <h2 class="center">Mode</h2>
+    <div class="card wide">
+        <button onclick="location.href='/actionpage?cmd=g'">Gyro On/Off</button>
+        <button onclick="location.href='/actionpage?cmd=c'">Calibration</button>
+        <button onclick="location.href='/actionpage?cmd=kbalance'">Balanced</button>
     </div>
-    <div class="btn" style="width:100%">
-        <button onclick="location.href='/actionpage?cmd=kbkL'">Back Left</button>
-        <button onclick="location.href='/actionpage?cmd=kbkR'">Back Right</button>
+    <h2>Actions</h2>
+    <div class="card wide">
+        <button onclick="location.href='/actionpage?cmd=ksit'">Sit</button>
+        <button onclick="location.href='/actionpage?cmd=kck'">Check</button>
+        <button onclick="location.href='/actionpage?cmd=kbuttUp'">Buttup</button>
+        <button onclick="location.href='/actionpage?cmd=khi'">Hello</button>
+        <button onclick="location.href='/actionpage?cmd=kstr'">Stretch</button>
+        <button onclick="location.href='/actionpage?cmd=kpee'">Pee</button>
+        <button onclick="location.href='/actionpage?cmd=kpu'">Push ups</button>
+        <button onclick="location.href='/actionpage?cmd=kvt'">Stepping</button>
+        <button onclick="location.href='/actionpage?cmd=lu'">Look up</button>
+        <button onclick="location.href='/actionpage?cmd=kbf'">Backflip</button>
+        <button onclick="location.href='/actionpage?cmd=kbdF'">Jumps</button>
+        <button onclick="location.href='/actionpage?cmd=krc'">Recover</button>
+        <button class="redBG" style="width: 100%" onclick="location.href='/actionpage?cmd=d'">STOP</button>
     </div>
-    <div class="btn center" style="width: 100%;">
-        <button onclick="location.href='/actionpage?cmd=kbk'">Back</button>
-    </div>
-    <h4 class="center">Mode</h4>
-    <div class="btn-group" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=g'">GyroOn/Off</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=c'">Calibration</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kbalance'">Balanced</button>
-    </div>
-    <h4 class="center">Actions</h4>
-    <div class="btn-group" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=ksit'">Sit</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kck'">Check</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kbuttUp'">Buttup</button>
-    </div>
-    <div class="btn-group" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=khi'">Hello</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kstr'">Stretch</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kpee'">Pee</button>
-    </div>
-    <div class="btn-group" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kpu'">Push ups</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kvt'">Stepping</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=lu'">Look up</button>
-    </div>
-    <div class="btn-group" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kbf'">Backflip</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=kbdF'">Jumps</button>
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=rc'">Recover</button>
-    </div>
-    <div class="btn-group btn-stop" style="width:100%">
-        <button style="width: 100%" onclick="location.href='/actionpage?cmd=d'">STOP</button>
-    </div>
-    <h4 class="center">Custom command</h4>
+    <h2 class="center">Custom commands</h2>
     <form action="/action" method="get">
         <div class="center" style="width: 100%; margin-top: 25px;">
-            <input name="name" type="text" placeholder="input command"><input type="submit"/>
+            <input name="name" type="text" placeholder="input command"><input type="submit" value="Send"/>
         </div>
     </form>
     <script type="text/javascript">
